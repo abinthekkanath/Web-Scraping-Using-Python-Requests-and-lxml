0a7a8083c2da455203f1e4559c6fa8f38d2455c8
Abin Shaju T <abinshaju30@gmail.com>
2019-05-29 10:01:46 +0530

    Update musicroom.py

--- musicroom.py
+++ musicroom.py
@@ -4,10 +4,14 @@ from w3lib.html import remove_tags
 import json
 
 block=0
+count=0
 
 def targetPage(url):
 
-	
+	global count
+	count=count+1
+			
+	print("Item Count:",count)
 	response=requests.get(url)
 	
 	tree = html.fromstring(response.content)
@@ -38,6 +42,13 @@ def targetPage(url):
 		json.dump(musicroom, f, indent=2, ensure_ascii=False)
 
 def nextPage(url):
+	
+	global block
+
+	block=block+1
+	print("Block Number:",block)
+	print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+
 	response=requests.get(url)
 	
 	tree=html.fromstring(response.content)
@@ -46,28 +57,25 @@ def nextPage(url):
 	
 	head="https://www.musicroom.com/"
 	
-	count=0
 	
-	print(count)
+	
 	
 	for i in urllist:
 		par=head+i
 		print(par)
 		targetPage(par)
-		count=count+1
-		print(count)
+		
 
 	nexturl="".join(tree.xpath('//a[@id="ctl00_ctl00_ctl00_SiteContent_SiteContent_SiteContent_uxProducts_ProductBlocks_ctl26_ddPagerBottom_HyperLink3"]/@href'))
 	
 	head="https://www.musicroom.com/"
 	
+	print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 	if nexturl :
 		par=head+nexturl
 		print(par)
-		print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 		nextPage(par)
-		print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-		block=block+1
-		print(block)
+	
+		
 
 nextPage('https://www.musicroom.com/productlist/piano+sheet+music+and+songbooks/piano-sheet-music-songbooks.aspx')
